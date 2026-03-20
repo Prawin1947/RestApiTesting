1c50bbd44718d1712fd5d9e76bb12481f75a67f2
Praveen Mayakar <149757747+Prawin1947@users.noreply.github.com>
2026-03-20 13:28:43 +0530

    Reorganize test variables in TestDemo.java

--- src/test/java/dummyPackage/TestDemo.java
+++ src/test/java/dummyPackage/TestDemo.java
@@ -5,6 +5,8 @@ public class TestDemo {
     int test1 = 100;
     int test2 = 200;
     int test3 = 300;
-    int test6 = 600;
+    int test4 = 400;
     int test5 = 500;
+    int test6 = 600;
+  
 }
